13e5107d073a40e6ec70aa272b94174189303374
xiaobo <1968468227@qq.com>
2017-09-26 16:29:38 +0800

    修改判断题功能

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,6 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    // if("login"){
-    // 	res.redirect('/login');
-    // }
 
     res.render('index', { title: 'Express' });
 });
