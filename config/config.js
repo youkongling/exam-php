79e5e46c4b6ca0c3fd4234ea07f93810ed6ea2a8
xiaobo <1968468227@qq.com>
2017-10-08 18:03:07 +0800

    答案解析模块完成

--- config/config.js
+++ config/config.js
@@ -1,13 +1,13 @@
-var config = {
-	list : 'http://gaojun.s1.natapp.cc/nongshang/admin/index.php?api-app-api',
-	anwser : 'http://gaojun.s1.natapp.cc/nongshang/admin/index.php?api-app-api-getAnswer',
-	test : 'http://gaojun.s1.natapp.cc/nongshang/admin/index.php?api-app-api-getQuesList'
-}
-
 // var config = {
-// 	list : 'http://211.159.186.84/index.php?api-app-api',
-// 	anwser : 'http://211.159.186.84/index.php?api-app-api-getAnswer',
-// 	test : 'http://211.159.186.84/index.php?api-app-api-getQuesList'
+// 	list : 'http://gaojun.s1.natapp.cc/nongshang/admin/index.php?api-app-api',
+// 	anwser : 'http://gaojun.s1.natapp.cc/nongshang/admin/index.php?api-app-api-getAnswer',
+// 	test : 'http://gaojun.s1.natapp.cc/nongshang/admin/index.php?api-app-api-getQuesList'
 // }
+
+var config = {
+	list : 'http://211.159.186.84/index.php?api-app-api',
+	anwser : 'http://211.159.186.84/index.php?api-app-api-getAnswer',
+	test : 'http://211.159.186.84/index.php?api-app-api-getQuesList'
+}
 
 module.exports = config;
