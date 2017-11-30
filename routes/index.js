5b02c1af298cae37afd72fb3d73845f1ab496ded
xiaobo <1968468227@qq.com>
2017-11-30 22:29:25 +0800

    增加考试

--- routes/index.js
+++ routes/index.js
@@ -52,14 +52,12 @@ router.get('/confirm/:id', function(req, res, next) {
 /* GET home page. */
 router.get('/index/:id', function(req, res, next) {
     var _cookie = req.cookies;
-    // console.log(_cookie);
-    var id = req.params.id;
-    request(config.list + '&userid=' + id + '&jobid=' + _cookie.jobid, function(error, response, body) {
+    request(config.list + '&userid=' + _cookie.userid + '&jobid=' + _cookie.jobid, function(error, response, body) {
         if (error) {
             res.render('index');
         } else {
             var _body = JSON.parse(body);
-            console.log(_body)
+            console.log(_body);
             res.render('index', { data: _body.data, cookie: _cookie.number });
         }
     })
